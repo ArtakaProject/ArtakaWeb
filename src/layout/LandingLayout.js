af4bf096282c88aad11e1ae4b5b92a9f5598dfda
satrioadi96 <prawiro.96@gmail.com>
2021-12-05 13:12:44 +0700

    conflict done

--- src/layout/LandingLayout.js
+++ src/layout/LandingLayout.js
@@ -59,11 +59,7 @@ export default function LandingLayout(props) {
                 <div className="hidden md:flex">
                   <Link
                     to="/artaka/signin"
-<<<<<<< HEAD
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-=======
                     className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-purple-700 hover:bg-blue-700"
->>>>>>> 7146a8eb19eff6357e13647235cab0f091259fa4
                   >
                     Sign In
                   </Link>
