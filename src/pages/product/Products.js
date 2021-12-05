6e59728fbe76cc7f0ccf28ed07bf2fbe3fc0d873
satrioadi96 <prawiro.96@gmail.com>
2021-12-05 17:42:11 +0700

    pagination done

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -11,8 +11,8 @@ import config from '../../config/config';
 
 import { useHistory, Link } from "react-router-dom";
 
-//import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
-import Pagination from 'react-js-pagination';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+//import Pagination from 'react-js-pagination';
 
 
 
@@ -37,15 +37,7 @@ export default function Products() {
     const products = useSelector((state) => state.productState.products);
     const remain_stock = useSelector((state) => state.productState.remain_stock);
 
-    const page = {
-        activePage: 10
-    };
-    
-    function handlePageChange(pageNumber) {
-        console.log(`active page is ${pageNumber}`);
-        this.setState({ activePage: pageNumber });
-    }
-    
+
 
     useEffect(() => {
         fetchData();
@@ -204,16 +196,90 @@ export default function Products() {
                                         </tr>
                                     ))}
                                 </tbody>
-                                <div>
-                                    <Pagination
-                                        activePage={page.activePage}
-                                        itemsCountPerPage={10}
-                                        totalItemsCount={300}
-                                        pageRangeDisplayed={5}
-                                        onChange={handlePageChange.bind(this)}
-                                    />
-                                </div>
                             </table>
+                            <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+                                <div className="flex-1 flex justify-between sm:hidden">
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                                    >
+                                        Sebelumnya
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                                    >
+                                        Next
+                                    </a>
+                                </div>
+                                <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+                                    <div>
+                                        <p className="text-sm text-gray-700">
+                                            Daftar ke - <span className="font-medium">1</span> sampai <span className="font-medium">10</span> dari{' '}
+                                            <span className="font-medium">25</span> hasil
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                                            <a
+                                                href="#"
+                                                className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                                            >
+                                                <span className="sr-only">Sebelumnya</span>
+                                                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                                            </a>
+                                            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+                                            <a
+                                                href="#"
+                                                aria-current="page"
+                                                className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                1
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                2
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                3
+                                            </a>
+                                            <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+                                                ...
+                                            </span>
+                                            <a
+                                                href="#"
+                                                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                8
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                9
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                            >
+                                                10
+                                            </a>
+                                            <a
+                                                href="#"
+                                                className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                                            >
+                                                <span className="sr-only">Selanjutnya</span>
+                                                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                                            </a>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
