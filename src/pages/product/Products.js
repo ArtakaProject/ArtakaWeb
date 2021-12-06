688ccd64c48ed15846d6ccc2c067ed880fa56236
satrioadi96 <prawiro.96@gmail.com>
2021-12-06 12:57:02 +0700

    product edited

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -17,10 +17,11 @@ const columns = [
     { column: 'Nama' },
     { column: 'Harga' },
     { column: 'Deskripsi' },
-    { column: 'Kategori' },
+    /* { column: 'Kategori' }, */
     { column: 'Terjual' },
     { column: 'Stok' },
-    { column: 'Tersedia' }
+    { column: 'Tersedia' },
+    { column: 'Opsi/Edit'}
 ];
 
 function classNames(...classes) {
@@ -80,21 +81,18 @@ export default function Products() {
                                                 <div className="flex items-center">
                                                     <div className="flex-shrink-0 h-10 w-10">
                                                         <img className="h-10 w-10 rounded-full"
-                                                            src={`${config.urlImage}/${prod.images}`}
-                                                            alt="" />
+                                                            src={prod.images} /* `${config.urlImage}/${prod.images}` */ alt="" />
                                                     </div>
-
                                                     <div className="ml-4">
                                                         <div className="text-sm font-medium text-gray-900">{prod.name}</div>
-                                                        
+                                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prod.desc}</td>
                                                     </div>
                                                 </div>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap">
                                                 <div className="text-sm text-gray-900">Rp. {new Intl.NumberFormat('ID').format(prod.sell_cost)}</div>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prod.desc}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prod.category}</td>
+                                            {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prod.category}</td> */}
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{prod.quantity}</td>
                                             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{remain_stock}</td>
                                             <td>
@@ -102,6 +100,9 @@ export default function Products() {
                                                 <label class="switch toggle">
                                                     <span class="slider round"></span>
                                                 </label>
+                                            </td>
+                                            <td>
+                                                <span className="sr-only">...</span>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium ">
                                                 <Menu as="div" className="relative flex justify-end items-center ">
