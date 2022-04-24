439b5c8c9487140d782a24315daf3156f36b87ce
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-04-24 14:01:11 +0700

    update

--- src/api/api-product.js
+++ src/api/api-product.js
@@ -10,39 +10,6 @@ const findAll = async(payload)=>{
     }
 }
 
-/* const addProduct = async(payload)=>{
-    try {
-        const result = await axios.post(`${config.domain}/product/show`,payload);
-        return result;    
-    } catch (error) {
-        return error;
-    }
-}
-
-const editProduct = async(id, payload)=>{
-    try {
-        const result = await axios.put(`${config.domain}/product/show/${id}`,payload);
-        return result;    
-    } catch (error) {
-        return error;
-    }
-}
-
-const deleteProduct = async(id)=>{
-    try {
-        const result = await axios.delete(`${config.domain}/product/${id}`);
-        return  result;
-    } catch (error) {
-        return error;
-        
-    }
-} */
-
-
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     findAll
-    //addProduct,
-    //editProduct,
-    //deleteProduct
 }
